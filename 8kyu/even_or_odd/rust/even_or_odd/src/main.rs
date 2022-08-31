17668aac65332f656838ad53f597fc522ccceb01
atinyame <104745335+atinyame@users.noreply.github.com>
2022-08-31 13:57:53 -0500

    8kyu: Even or Odd, rust

---
+++ 8kyu/even_or_odd/rust/even_or_odd/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {}
+
+fn even_or_odd(i: i32) -> &'static str {
+    match i % 2 == 0 {
+        true => "Even",
+        _ => "Odd",
+    }
+}
+
+#[test]
+fn returns_expected() {
+    assert_eq!(even_or_odd(0), "Even");
+    assert_eq!(even_or_odd(2), "Even");
+    assert_eq!(even_or_odd(1), "Odd");
+    assert_eq!(even_or_odd(7), "Odd");
+    assert_eq!(even_or_odd(-1), "Odd");
+}
