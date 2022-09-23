4c7bfbc9b14f9d6950846895f9597c37b208c4f9
atinyame <104745335+atinyame@users.noreply.github.com>
2022-09-23 17:46:47 -0500

    8kyu: Remove String Spaces, c#

---
+++ 8kyu/remove_string_spaces/c#/SpacesRemover/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Solution
+{
+    public static class SpacesRemover
+    {
+        public static string NoSpace(string input)
+        {
+            return input.Replace(" ", "");
+        }
+    }
+}
