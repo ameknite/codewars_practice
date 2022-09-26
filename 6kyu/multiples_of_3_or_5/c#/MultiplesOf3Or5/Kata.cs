cf6c17c1a2b7c23e452d1a16f487295b84d1ea2d
atinyame <104745335+atinyame@users.noreply.github.com>
2022-09-25 19:20:12 -0500

    6kyu: Multiples of 3 or 5, c#

---
+++ 6kyu/multiples_of_3_or_5/c#/MultiplesOf3Or5/Kata.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+
+public static class Kata
+{
+    public static int Solution(int value)
+    {
+
+        return value <= 0 ? 0 : Enumerable.Range(1, value - 1).Where(x => (x % 3 == 0 || x % 5 == 0)).Sum();
+    }
+}
