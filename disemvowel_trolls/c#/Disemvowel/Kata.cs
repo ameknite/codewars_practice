f743048945fb5e101d5209008bb853fd10de2221
atinyame <104745335+atinyame@users.noreply.github.com>
2022-09-28 13:44:26 -0500

    7kyu: Disemvowel Trolls, c#

---
+++ disemvowel_trolls/c#/Disemvowel/Kata.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static string Disemvowel(string str)
+    {
+        return String.Join("", str.Where(c => !"aeiouAEIOU".Contains(c)));
+    }
+}
