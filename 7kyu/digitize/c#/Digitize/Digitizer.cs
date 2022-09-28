5d528681886ed24344db2e12dc00c92a9244a4b3
atinyame <104745335+atinyame@users.noreply.github.com>
2022-09-28 14:21:32 -0500

    8kyu: Convert number to reversed array of digits. - c#

---
+++ 7kyu/digitize/c#/Digitize/Digitizer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Solution
+{
+    class Digitizer
+    {
+        public static long[] Digitize(long n)
+        {
+            if (n == 0)
+            {
+                return new long[] { 0 };
+            }
+            IEnumerable<long> output = new long[] { };
+            while (n != 0)
+            {
+                output = output.Append(n % 10);
+                n /= 10;
+            }
+            return output.ToArray();
+        }
+    }
+}
