7a88c4bf59040243d8635e8ecba8534fefb3baa3
atinyame <104745335+atinyame@users.noreply.github.com>
2022-09-29 21:08:07 -0500

    8kyu: Find Multiples of a Number, c#

---
+++ 8kyu/find_multiples_of_a_number/c#/FindMultiples/Kata.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+public class Kata
+{
+    public static List<int> FindMultiples(int integer, int limit)
+    {
+        return Enumerable.Range(1, limit / integer).Select(x => x * integer).ToList();
+    }
+}
