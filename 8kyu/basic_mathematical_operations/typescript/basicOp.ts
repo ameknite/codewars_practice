65a89494bf80514df9f71e656e14320f45b3c897
atinyame <104745335+atinyame@users.noreply.github.com>
2022-10-11 21:10:26 -0500

    8kyu: Basic Mathematical Operations, typescript

---
+++ 8kyu/basic_mathematical_operations/typescript/basicOp.ts
@@ -0,0 +1,14 @@
+export function basicOp(operation: string, value1: number, value2: number): number {
+    switch (operation) {
+        case '+':
+            return value1 + value2
+        case '-':
+            return value1 - value2
+        case '*':
+            return value1 * value2
+        case '/':
+            return value1 / value2
+        default:
+            throw new Error("Invalid Operator");
+    }
+}
