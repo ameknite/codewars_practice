db3d53e6eaba3c4e8037d78462f491dd59d27fcf
atinyame <104745335+atinyame@users.noreply.github.com>
2022-10-11 20:47:52 -0500

    8kyu: Returning Strings, rust

--- 8kyu/returning_strings/rust/greet/src/lib.rs
+++ 8kyu/returning_strings/rust/greet/src/lib.rs
@@ -1,18 +1,3 @@
 fn greet(name: &str) -> String {
     format!("Hello, {name} how are you doing today?")
 }
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn basic_tests() {
-        assert_eq!(greet("Ryan"), "Hello, Ryan how are you doing today?");
-        assert_eq!(
-            greet("Shingles"),
-            "Hello, Shingles how are you doing today?"
-        );
-    }
-}
