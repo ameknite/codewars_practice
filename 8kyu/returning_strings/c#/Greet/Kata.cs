9a0560a2ff45f372bdec3d081cca8f41b33a48fe
atinyame <104745335+atinyame@users.noreply.github.com>
2022-10-11 20:54:09 -0500

    8kyu: Returning Strings, c#

---
+++ 8kyu/returning_strings/c#/Greet/Kata.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public static class Kata
+{
+    public static string Greet(string name)
+    {
+        return string.Format("Hello, {0} how are you doing today?", name);
+    }
+}
