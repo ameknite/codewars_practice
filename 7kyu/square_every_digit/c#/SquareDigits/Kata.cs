3efbd0f3d34753727f0bfe558f80f0dcf4a104ff
atinyame <104745335+atinyame@users.noreply.github.com>
2022-10-14 19:41:14 -0500

    7kyu: Square Every Digit, c#

---
+++ 7kyu/square_every_digit/c#/SquareDigits/Kata.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static int SquareDigits(int n)
+    {
+        return Int32.Parse(String.Concat(n.ToString().Select(c => (int)Math.Pow(char.GetNumericValue(c), 2))));
+    }
+}
